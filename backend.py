10d040533e7be3f84be21a2a85f2df6fac2853cd
Harvinder Singh <harvindersinghdiwan@gmail.com>
2020-05-03 02:52:45 +0530

    third commit

--- backend.py
+++ backend.py
@@ -1,41 +1,54 @@
 from flask import Flask,request
 import requests
 app=Flask(__name__)
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def home():
+    x=requests.get("http://192.168.43.70/home.html")
+    return x.text
+@app.route('/home.html', methods=["GET","POST"])
+def homenew():
     x=requests.get("http://192.168.43.70/home.html")
     return x.text
 @app.route('/Instance1.html' , methods=['GET','POST'])
 def Instance1():
     x=requests.get("http://192.168.43.70/Instance1.html")
-    #print(x.content)
-    return i = Image.open(BytesIO(x.content))
+    return x.text
 @app.route('/Instance2.html' , methods=['GET','POST'])
 def Instance2():
+    x=requests.get("http://192.168.43.70/Instance2.html")
+    return x.text
+def Iquery():
     if request.method=="POST":
         NetworkName=request.form["Network Name"]
         SubnetName=request.form["Subnet Name"]
         InstanceName=request.form["Instance Name"]
-        
-    y=requests.get("http://192.168.43.70/Instance2.html")
-    return y.text
+        return list([MetworkName,SubnetName,InstanceName])
+@app.route('/config.html' , methods=['GET','POST'])
+def config():
+    return "<h>{}</h>".format(Iquery())
 @app.route('/vpctype.html' , methods=['GET','POST'])
 def vpctype():
-    vpctype=requests.get("http://192.168.43.70/vpctype.html")
-    return vpctype.text
+    x=requests.get("http://192.168.43.70/vpctype.html")
+    return x.text
 @app.route('/onevpc.html' , methods=['GET','POST'])
 def onevpc():
+    x=requests.get("http://192.168.43.70/onevpc.html")
+    return x.text
+def VSquery():  
     if request.method=="POST":
         MPIPv4S=request.form["IPv4 CIDR block"]
         PVPCNameS=request.form["VPC Name"]
         PSubnetIPV4S=request.form["Public subnet's IPv4 CIDR"]
-        PSubnetNameS=request.form["Public subnet name"]
-        
-        return "<h1>{}</h1>".format(z)
-    onevpc=requests.get("http://192.168.43.70/onevpc.html")
-    return onevpc.text
+        PSubnetNameS=request.form["Public subnet name"] 
+        return list([MIPV4S,PVPCNameS,PSubnetIPV4S,PSubnetNameS])
+@app.route('/single.html' , methods=['GET','POST'])
+def single():
+    return VSquery()
 @app.route('/vpcboth.html' , methods=['GET','POST'])
 def vpcboth():
+    x=requests.get("http://192.168.43.70/vpcboth.html")
+    return x.text
+def VBquery():
     if request.method=="POST":
         MPIPv4=request.form["IPv4 CIDR block"]
         PVPCName=request.form["VPC Name"]
@@ -43,16 +56,44 @@ def vpcboth():
         PSubnetName=request.form["Public subnet name"]
         PrSubnetIPV4=request.form["Private subnet's IPv4 CIDR"]
         PrSubnetName=request.form["Private subnet name"]
-        return "<h1>{}</h1>".format(z)
-    vpcboth=requests.get("http://192.168.43.70/vpcboth.html")
-    return vpcboth.text
+        return list([MPIPv4,PVPCName,PSubnetIPV4,PSubnetName,PrSubnetIPV4,PrSubnetName])
+@app.route('/both.html' , methods=['GET','POST'])
+def both():
+    return VBquery()
 @app.route('/colab.html' , methods=['GET','POST'])
 def colab():
     colab=requests.get("http://192.168.43.70/vpctype.html")
     return colab.text
-@app.route('/apacheweb.html' , methods=['GET','POST'])
-def apacheweb():
-    colab=requests.get("http://192.168.43.70/apacheweb.html")
-    return colab.text
+@app.route('/detectfaces.html' , methods=['GET','POST'])
+def detectfaces():
+    x=requests.get("http://192.168.43.70/detectfaces.html")
+    return x.text
+@app.route('/detect_faces.html' , methods=['GET','POST'])
+def detect_faces():
+    pass
+@app.route('/countfaces.html' , methods=['GET','POST'])
+def countfaces():
+    x=requests.get("http://192.168.43.70/countfaces.html")
+    return x.text
+@app.route('/count_faces.html' , methods=['GET','POST'])
+def count_faces():
+    pass
+@app.route('/wordpress.html' , methods=['GET','POST'])
+def wordpress():
+    x=requests.get("http://192.168.43.70/wordpress.html")
+    return x.text
+def Wquery():
+    if request.method=="POST":
+        WordPressInstanceName=request.form["WordPress Instance Name"]
+        DataBaseInstanceName=request.form["DataBase Instance Name"]
+        DataBaseRootPassword=request.form["DataBase Root Password"]
+        DataBaseUser=request.form["DataBase User"]
+        DataBaseUserPassword=request.form["DataBase User Password"]
+        DataBaseName=request.form["DataBase Name"]
+        return list([WordPressInstanceName,DataBaseInstanceName,DataBaseRootPassword,DataBaseUser,DataBaseUserPassword,DataBase])
+@app.route('/pressos.html' , methods=['GET','POST'])
+def pressos():
+    return Wquery()
+
 if __name__=='__main__':
     app.run()
